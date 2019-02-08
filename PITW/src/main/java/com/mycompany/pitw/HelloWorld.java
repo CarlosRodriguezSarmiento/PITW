53cfb4f396d58e68e563659f14dbc5716a25c039
BOG-A209-E-005 <BOG-A209-E-005@RGH-LS6-05.UMD.LOCAL.EDU>
2019-02-07 21:32:26 -0500

    Hola Mundo

---
+++ PITW/src/main/java/com/mycompany/pitw/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.mycompany.pitw ;
+import org.joda.time.LocalTime ;
+public class HelloWorld {
+    public static void main ( String Args [ ] ) {
+        LocalTime currentTime = new LocalTime ( ) ;
+        System.out.println ( " The Current Local Time is: " + currentTime ) ;
+        System.out.println ( " Hola Mundo " ) ;
+    }
+}
